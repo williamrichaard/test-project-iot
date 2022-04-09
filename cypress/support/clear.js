b7e0b8a13e70d21413f044195fedb39e6657f790
williamrichaard <wrichaard@hotmail.com>
2022-04-09 15:16:10 -0300

    Adicionando melhorias no html report

--- cypress/support/clear.js
+++ cypress/support/clear.js
@@ -1,5 +1,6 @@
 const rimraf = require('rimraf');
 
+rimraf.sync('cypress/videos/spec/*.mp4')
 rimraf.sync('report/json/*')
 rimraf.sync('report/html/*')
 rimraf.sync('report/screenshots/*')
--- cypress/support/reporter.js
+++ cypress/support/reporter.js
@@ -2,5 +2,17 @@ const report = require('multiple-cucumber-html-reporter')
 
 report.generate({
     jsonDir: 'report/json',
-    reportPath: 'report/html'
+    reportPath: 'report/html',
+    displayDuration: true,
+    pageTitle: 'System-Test Report',
+    reportName: `System-Test Report - ${new Date().toLocaleString()}`,
+    metadata: {
+        browser: {
+            name: 'chrome'
+        },
+        device: 'remote',
+        platform: {
+            name: 'windows'
+        }
+    }
 })
