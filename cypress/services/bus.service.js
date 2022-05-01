cbf903be6259b73a3a3f158d1b792f924ed4d207
williamrichaard <wrichaard@hotmail.com>
2022-05-01 18:19:15 -0300

    Adicionando service object

--- cypress/services/bus.service.js
+++ cypress/services/bus.service.js
@@ -11,25 +11,25 @@ export class Bus extends Rest {
 
     static criar_bus(bus_type) {
         let body = Factory.postBus(bus_type)
-        return super.httpRequestWithBody('POST', URL_BUS, body).then((res) => { console.log(res) })
+        return super.httpRequestWithBody('POST', URL_BUS, body)
     }
 
     static criar_parada(busstop_type) {
         let body = Factory.postBusstop(busstop_type)
-        return super.httpRequestWithBody('POST', URL_BUSSTOP, body).then((res) => { console.log(res) })
+        return super.httpRequestWithBody('POST', URL_BUSSTOP, body)
     }
 
     static post_check(check_type) {
         let body = Factory.postCheck(check_type)
-        return super.httpRequestWithBody('POST', URL_CHECK, body).then((res) => { console.log(res) })
+        return super.httpRequestWithBody('POST', URL_CHECK, body)
     }
 
     static buscar_parada_id() {
         let id = 1
-        return super.httpRequestWithoutBody('GET', `${URL_NEXTBUSES}/${id}`).then((res) => { console.log(res) })
+        return super.httpRequestWithoutBody('GET', `${URL_NEXTBUSES}/${id}`)
     }
 
     static buscar_bus_id() {
-        return super.httpRequestWithoutBody('GET', URL_BUS_ID).then((res) => { console.log(res) })
+        return super.httpRequestWithoutBody('GET', URL_BUS_ID)
     }
 }
