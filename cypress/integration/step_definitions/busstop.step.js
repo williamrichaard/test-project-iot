0947897f3be157787662c1bb6d5bacf97fa3ab98
williamrichaard <wrichaard@hotmail.com>
2022-05-01 18:07:44 -0300

    Adicionando step busstop

---
+++ cypress/integration/step_definitions/busstop.step.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+import {When, Then} from 'cypress-cucumber-preprocessor/steps'
+import {Bus} from '../../services/bus.service'
+
+When(`criar parada por {string}`, (busstop_type) => {
+	Bus.criar_parada(busstop_type).then(post_response => {
+        cy.wrap(post_response).as('Response')
+    })
+});
+
+Then(`deve ser respondido o schema {string} com status {int}`, (schema, status) => {
+    cy.get('@Response').then(res => {
+        cy.contractValidation(res, schema, status).then(valid => {
+            expect(valid).to.be.true
+        })
+    })
+});
