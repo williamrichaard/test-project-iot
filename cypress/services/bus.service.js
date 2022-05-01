a2bd0bfedee0c70ae5aa48f3892e572531783a0a
williamrichaard <wrichaard@hotmail.com>
2022-05-01 18:08:55 -0300

    Adicionando service object

--- cypress/services/bus.service.js
+++ cypress/services/bus.service.js
@@ -1,31 +1,32 @@
 import Rest from "./common/_bus.service"
+import { Factory } from '../fixtures/factory'
 
 const URL_BUS = '/bus'
+const URL_BUSSTOP = '/busstop'
 const URL_CHECK = '/check'
-const URL_BUS_ID = '/bus/1'
-const URL_NEXTBUSES = '/busstop/nextBuses/1'
+const URL_BUS_ID = '/bus'
+const URL_NEXTBUSES = '/busstop/nextBuses'
 
 export class Bus extends Rest {
 
-    static criar_bus() {
-        let body = {
-            "name": "fortaleza",
-            "busNumber": 0,
-            "route": "string"
-        }
+    static criar_bus(bus_type) {
+        let body = Factory.postBus(bus_type)
         return super.httpRequestWithBody('POST', URL_BUS, body).then((res) => { console.log(res) })
     }
 
-    static post_check() {
-        let body = {
-            "bus_id": 1,
-            "busStop_id": 1
-        }
+    static criar_parada(busstop_type) {
+        let body = Factory.postBusstop(busstop_type)
+        return super.httpRequestWithBody('POST', URL_BUSSTOP, body).then((res) => { console.log(res) })
+    }
+
+    static post_check(check_type) {
+        let body = Factory.postCheck(check_type)
         return super.httpRequestWithBody('POST', URL_CHECK, body).then((res) => { console.log(res) })
     }
 
     static buscar_parada_id() {
-        return super.httpRequestWithoutBody('GET', URL_NEXTBUSES).then((res) => { console.log(res) })
+        let id = 1
+        return super.httpRequestWithoutBody('GET', `${URL_NEXTBUSES}/${id}`).then((res) => { console.log(res) })
     }
 
     static buscar_bus_id() {
